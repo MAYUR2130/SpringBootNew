a146e6512298e79067b3a0d86b074a1406de920e
MAYUR2130 <MAYURKHAIRNAR21@GMAIL.COM>
2022-12-14 11:58:59 +0530

    Update ProductController.java

--- src/main/java/com/aws/controller/ProductController.java
+++ src/main/java/com/aws/controller/ProductController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
 @RestController
 public class ProductController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@RequestMapping(value="/testApi",method=RequestMethod.GET)
     ProductDto home() {
-         
-        ProductDto product=new ProductDto("Product",100);
+         System.out.println("secret is "+ environment.getProperty("testsecretkey")); 
+        ProductDto product=new ProductDto(environment.getProperty("testsecretkey"),100);
         return product;
     }
 	
